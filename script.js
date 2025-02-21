efb1354c47d07bdefeb91d3954d2f0e38519a492
Nabeel-vu <bc220423716nah@vu.edu.pk>
2025-02-21 16:59:45 +0500

    fixed issue

--- script.js
+++ script.js
@@ -12,24 +12,36 @@ function selectPackage(packageElem) {
     if (whatsappBtn) {
         whatsappBtn.disabled = false;
     }
+
+    // Remove highlight if previously marked
+    selectedPackage.style.border = "";
 }
 
 function openWhatsApp() {
-    var characterId = document.getElementById('character-id').value;
+    var characterIdInput = document.getElementById('character-id');
 
     if (!selectedPackage) {
         alert('Please select the package you wish to purchase.');
+        // Highlight the packages section
+        document.getElementById('packages-section').style.border = "2px solid red";
         return;
+    } else {
+        document.getElementById('packages-section').style.border = "";
     }
 
-    if (!characterId || characterId.length < 10) {
+    if (!characterIdInput.value || characterIdInput.value.length < 10) {
         alert('Please enter a valid PUBG Character ID.');
+        // Highlight the input field
+        characterIdInput.style.border = "2px solid red";
+        characterIdInput.focus();
         return;
+    } else {
+        characterIdInput.style.border = "";
     }
 
     var packageName = selectedPackage.querySelector('.package-title').textContent;
     var packagePrice = selectedPackage.querySelector('.package-price').textContent;
-    var message = "I am interested in purchasing " + packageName + " UC, Price: " + packagePrice + ". My PUBG Player ID is: " + characterId;
+    var message = "I am interested in purchasing " + packageName + " UC, Price: " + packagePrice + ". My PUBG Player ID is: " + characterIdInput.value;
 
     var whatsappUrl = "https://api.whatsapp.com/send/?phone=923023336555&text=" + encodeURIComponent(message);
 
